da670fa5b709ce3b27df7a2551b0c54922e3bd2b
BariOm <omar.bariffi@gmail.com>
2025-05-23 10:43:02 +0200

    Fix live, classifica provvisoria

--- live.py
+++ live.py
@@ -106,6 +106,6 @@ def show_live():
 
             for i, (nome, club, totale) in enumerate(classifica):
                 col = columns[i // 10]
-                col.markdown(f"<div style='font-size:16px;'>{i+1}. <b>{nome} — </b><i>{club}</i> — <b>{totale:.3f}</b></div>", unsafe_allow_html=True)
+                col.markdown(f"<div style='font-size:16px;'>{i+1}. <b>{nome} — {totale:.3f}</b></br><i>{club}</i></div>", unsafe_allow_html=True)
 
     conn.close()
