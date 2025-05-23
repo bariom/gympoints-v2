444950947bc1ce1b81065d373a27103a30966433
BariOm <omar.bariffi@gmail.com>
2025-05-23 09:57:38 +0200

    Fix classifica provvisoria sul live

--- live.py
+++ live.py
@@ -5,8 +5,7 @@ from db import get_connection
 from streamlit_autorefresh import st_autorefresh
 
 def show_live():
-    st.set_page_config(layout="wide")
-    st_autorefresh(interval=5000, key="refresh_live")
+    st_autorefresh(interval=2000, key="refresh_live")
 
     conn = get_connection()
     c = conn.cursor()
